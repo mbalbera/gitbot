c184834546d651eff051a2d042bf40c705e51211
mbalbera <50467671+mbalbera@users.noreply.github.com>
2025-05-07 15:16:00 -0400

    Update alterable_file.py

--- alterable_file.py
+++ alterable_file.py
@@ -1,3 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def chunks_of_five(arr):
+    for i in range(0, len(arr), 5):
+        yield arr[i:i + 5]
+
+
 my_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
 
 with ThreadPoolExecutor(max_workers=5) as executor:
